51b5ee2f25bd2f3b4e1ce663e48b560a25e14dc2
Daniel Madera <dmadera0@gmail.com>
2020-06-09 20:06:43 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,13 +6,13 @@ end
 
 def using_insert(list_of_programming_languages, anothoer_language)
   @list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
-  @anothoer_language = ["Python"]
+  @anothoer_language = "Python"
   @newarray = @list_of_programming_languages.insert(4, @anothoer_language)
 end
 
 def using_uniq(haircuts)
   @haircuts = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
-  new_array = haircuts.unique
+  new_array = haircuts.unique!
 end
 
 def using_flatten(instruments)
