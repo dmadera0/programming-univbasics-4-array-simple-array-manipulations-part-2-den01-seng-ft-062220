57d54b6163262783b9e8ba5d007a392b9be3d719
Daniel Madera <dmadera0@gmail.com>
2020-06-09 19:37:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -7,4 +7,4 @@ end
 def using_insert(list_of_programming_languages, anothoer_language)
   @list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
   @anothoer_language = ["Python"]
-  @newarray =
+  @newarray = @list_of_programming_languages
