263728e61229d9c2046f534d5baf1687d24d5a02
Daniel Madera <dmadera0@gmail.com>
2020-06-09 20:10:47 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -12,7 +12,7 @@ end
 
 def using_uniq(haircuts)
   @haircuts = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
-  new_array = haircuts.unique!
+  new_array = haircuts.unique
 end
 
 def using_flatten(instruments)
